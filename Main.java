f02a03fe3471cab4a52af084c34b7f4f18bfcd7a
David Cosby <60022092@ironschools.org>
2019-03-22 08:22:18 -0600

    property class up and running

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+
+
 class Main {
 	public static void main(String[] args) {
-		
+		Property boardwalk = new Property();
+		boardwalk.price = 200;
 	}
 }
---
+++ Property.java
@@ -0,0 +1,15 @@
+class Property {
+	String color = null;
+	String name = null;
+	boolean railroad = false;
+	int price = 0;
+	int mortgage = 0;
+	int houseCost = 0;
+	
+	int houses = 0;
+	int hotels = 0;
+	int[] houseRent = new int[]{0, 0, 0, 0};
+	int hotelRent = 0;
+
+}
+
