064eebb1bcfdef053f4eabf6396e7588eaadc7db
Justin Garnica <justin.garnica@suusuccess.org>
2019-03-28 08:36:14 -0600

    yay!!!!!! all done.  finished. just needs spell check.

--- PropertyData.java
+++ PropertyData.java
@@ -102,7 +102,127 @@ class PropertyData {
 		k_a.houseRent = new int[]{18, 90, 250, 700, 875};
 		k_a.hotelRent = 1050;
 		properties.add(k_a);
-
+		
+		Property in_a = new Property("red", "Indiana avenue");
+		in_a.price = 220;
+		in_a.mortgage = 110;
+		in_a.housePrice = 150;
+		in_a.houseRent = new int[]{18, 90, 250, 700, 875};
+		in_a.hotelRent = 1050;
+		properties.add(in_a);
+		
+		Property il_a = new Property("red", "Illinois avenue");
+		il_a.price = 240;
+		il_a.mortgage = 120;
+		il_a.housePrice = 150;
+		il_a.houseRent = new int[]{20, 100, 300, 750, 925};
+		il_a.hotelRent = 1100;
+		properties.add(il_a);
+		
+		Property a_a = new Property("yellow", "atlantic avenue");
+		a_a.price = 260;
+		a_a.mortgage = 130;
+		a_a.housePrice = 150;
+		a_a.houseRent = new int[]{22, 110, 330, 800, 975};
+		a_a.hotelRent = 1150;
+		properties.add(a_a);
+		
+		Property ve_a = new Property("yellow", "Ventor avenue");
+		ve_a.price = 260;
+		ve_a.mortgage = 130;
+		ve_a.housePrice = 150;
+		ve_a.houseRent = new int[]{22, 110, 330, 800, 975};
+		ve_a.hotelRent = 1150;
+		properties.add(ve_a);
+		
+		Property m_g = new Property("yellow", "Marvin gardens");
+		m_g.price = 280;
+		m_g.mortgage = 140;
+		m_g.housePrice = 150;
+		m_g.houseRent = new int[]{24, 120, 360, 850, 1025};
+		m_g.hotelRent = 1200;
+		properties.add(m_g);
+		
+		Property p_a = new Property("green", "Pacific avenue");
+		p_a.price = 300;
+		p_a.mortgage = 150;
+		p_a.housePrice = 150;
+		p_a.houseRent = new int[]{26, 130, 390, 900, 1100};
+		p_a.hotelRent = 1275;
+		properties.add(p_a);
+		
+		Property n_c_a = new Property("green", "North Carolina avenue");
+		n_c_a.price = 300;
+		n_c_a.mortgage = 150;
+		n_c_a.housePrice = 150;
+		n_c_a.houseRent = new int[]{26, 130, 390, 900, 1100};
+		n_c_a.hotelRent = 1275;
+		properties.add(n_c_a);
+		
+		Property pe_a = new Property("green", "Pensylvania avenue");
+		pe_a.price = 320;
+		pe_a.mortgage = 160;
+		pe_a.housePrice = 150;
+		pe_a.houseRent = new int[]{28, 150, 450, 1000, 1200};
+		pe_a.hotelRent = 1400;
+		properties.add(pe_a);
+		
+		Property p_p = new Property("dark blue", "Park place");
+		p_p.price = 350;
+		p_p.mortgage = 175;
+		p_p.housePrice = 175;
+		p_p.houseRent = new int[]{35, 175, 500, 1100, 1300};
+		p_p.hotelRent = 1500;
+		properties.add(p_p);
+		
+		Property b = new Property("dark blue", "Boardwalk");
+		b.price = 400;
+		b.mortgage = 200;
+		b.housePrice = 175;
+		b.houseRent = new int[]{50, 200, 600, 1400, 1700};
+		b.hotelRent = 2000;
+		properties.add(b);
+		
+		Property r_r = new Property("railroad", "Reading railroad");
+		r_r.railroad = true;
+		// add seperate railroad prices
+		r_r.price = 200;
+		r_r.mortgage = 100;
+		properties.add(r_r);
+		
+		Property p_r = new Property("railroad", "pensylvania railroad");
+		p_r.railroad = true;
+		// add seperate railroad prices
+		p_r.price = 200;
+		p_r.mortgage = 100;
+		properties.add(p_r);
+		
+		Property b_r = new Property("railroad", "B. & O. railroad");
+		b_r.railroad = true;
+		// add seperate railroad prices
+		b_r.price = 200;
+		b_r.mortgage = 100;
+		properties.add(b_r);
+		
+		Property s_r = new Property("railroad", "Short line railroad");
+		s_r.railroad = true;
+		// add seperate railroad prices
+		s_r.price = 200;
+		s_r.mortgage = 100;
+		properties.add(s_r);
+		
+		Property e_c = new Property("utility", "Electric company");
+		e_c.utility = true;
+		// add rent prices 
+		e_c.price = 150;
+		e_c.mortgage = 75;
+		properties.add(e_c);
+		
+		Property w_w = new Property("utility", "Water works");
+		w_w.utility = true;
+		// add rent prices 
+		w_w.price = 150;
+		w_w.mortgage = 75;
+		properties.add(w_w);
 	}
-	
 }
