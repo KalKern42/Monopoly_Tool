67c9877b43034ea9e98c30563247fdab358cb4d9
Kal Kern <student@HSiMac4-6.local>
2019-03-28 09:05:00 -0600

    The start of the commands. Current Commands: Kill <player> & "let me out"(the way to end the loop)

---
+++ Commands.java
@@ -0,0 +1,41 @@
+/*
+Author: Kal Kern
+Date: 3/28/19
+
+the structure needed for commands to operate the 
+*/
+
+import java.util.Scanner;
+
+class Commands {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int stayIn = 0;
+		String usrImpt = "";
+		String commTarget = "";
+		
+		System.out.println("Begining Monopoly Commands..... ");
+		System.out.println("(type: 'let me out' to leave)");
+				
+		while (stayIn == 0) {
+			System.out.print("Monopoly User: ");
+			usrImpt = input.nextLine();
+/* these are going to be the commands. 
+Adding a new comand looks like:
+elif (usrImpt.contains(command name){
+	everything the command does
+}*/
+			if (usrImpt.contains("let me out")) {
+				stayIn = 1;
+			}else if (usrImpt.contains("kill")) {
+				commTarget = commandMod(usrImpt, 4);
+				System.out.println(commTarget);//this line is just a test
+			}
+		}
+	}
+	public static String commandMod(String command, int commLenth){
+		//some commands have specific targets such as players or properties, this method is designed to "grab" such things
+		String modd = command.substring(commLenth + 1);
+		return(modd);		
+	}
+}
