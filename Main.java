1705c0b5e905b0ea7f5292c38adba7d832db0dd2
Justin Garnica <justin.garnica@suusuccess.org>
2019-04-01 08:30:08 -0600

    it works now for some reason. no Idea why.added two words and deleted one.

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
-import java.util.SetUp;
 
-class Main {
+
+public class Main {
 	static PropertyData properties = new PropertyData();
 	static PlayerData players = new PlayerData();
 		
 	public static void main(String[] args) {
 		
-		SetUp();
+		SetUp.setUp();
 	}
 }
 
--- SetUp.java
+++ SetUp.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class SetUp {
 	
-	public SetUp(){
+	public static void setUp(){
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("How many players will be enjoying my company?");
+		System.out.print("How many players will be enjoying my company? \n");
 		int p = input.nextInt();
-		for (int i = 0; i <= p; i++) {
-			System.out.print("What is the name of player" + i + "?");
-			String name = input.nextLine();
+		
+		for (int i = 1; i <= p; i++) {
+			
+			System.out.print("What is the name of player " + i + "? \n");
+			String name = input.next();
 		}
 	}
 }
