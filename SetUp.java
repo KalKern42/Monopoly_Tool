73a4a7638da602506e943b87172312407f8173b8
Justin Garnica <justin.garnica@suusuccess.org>
2019-04-03 10:19:01 -0600

    added illegal names

--- SetUp.java
+++ SetUp.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 import javax.lang.model.element.*;
+import java.util.ArrayList;
 
 public class SetUp {
 	
 	public static void setUp(){
 		Scanner input = new Scanner(System.in);
+		
+		ArrayList<String> illegalNames = new ArrayList<String>();
+		illegalNames.add("kill");
+		illegalNames.add("help");
+			
+		
 		
 		System.out.print("How many players will be enjoying my company?  I will not allow more than 6 or less than 2.  \n");
 		int p = input.nextInt();
@@ -16,9 +23,18 @@ public class SetUp {
 		}
 		
 		for (int i = 1; i <= p; i++) {
-			
-			System.out.print("What is the name of player " + i + "? \n");
-			String name = input.next();
+			String name = "";
+			do {
+				if (illegalNames.contains(name)) {
+					System.out.print("That is not allowed.  ");
+				}
+				System.out.print("What is the name of player " + i + "? \n");
+				name = input.next().toLowerCase();
+				
+				
+			} while (illegalNames.contains(name));
+			illegalNames.add(name);
+
 		}
 	}
 }
