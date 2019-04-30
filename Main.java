add0d0cd17e47f7ea86b010829beb9749e918071
Justin Garnica <justin.garnica@suusuccess.org>
2019-04-30 09:09:08 -0600

    got some stuff gave up on brown

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 class Main {
 	static PropertyData properties = Data.properties;
 	static PlayerData players = Data.players;
-	static CommandMethods commands = Data.commands;
+	static CommandMethods commands = Data.commands;	
 
 	
 	public static void main(String[] args) {
